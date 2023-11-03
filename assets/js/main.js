cf992939c02045e787344be1edc88d2e19e80ab8
keviinmiichael <ghiokevin@gmail.com>
2023-11-03 16:13:29 +0100

    :

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,21 @@ import { turn } from './scoreHandler.js';
 
 cardCreator();
 
-const cards = document.querySelectorAll(".card");
+const reload = () =>{
+  location.reload();
+}
 
+const cards = document.querySelectorAll(".card");
+const cross = document.querySelector(".icon-button")
+const modalButton = document.querySelector(".button")
+const footerButton = document.querySelector(".in-footer")
 
 for (const card of cards) {
   card.addEventListener("click", turn);
 }
 
 
+cross.addEventListener("click", reload)
+modalButton.addEventListener("click", reload)
+footerButton.addEventListener("click", reload)
 
--- assets/js/scoreHandler.js
+++ assets/js/scoreHandler.js
@@ -2,34 +2,62 @@ import Buffer from "./buffer.js";
 
 const showAttemps = document.querySelector(".attempst");
 const showScore = document.querySelector(".score");
-
-const buffer = new Buffer();
 let score = 0;
 let attempts = 0;
 
+const buffer = new Buffer();
+
+const solveDifference = () =>{
+  setTimeout(() => {
+    buffer.rejected();
+    attempts++;
+    showAttemps.innerHTML = `Attempts: ${attempts}`
+    buffer.clearBuffer();
+  }, 1000);
+}
+
+const solveCoincidence = () =>{
+  buffer.match();
+  score++;
+  attempts++;
+  showScore.innerHTML = `Score: ${score}`
+  showAttemps.innerHTML = `Attempts: ${attempts}`
+  buffer.clearBuffer();
+}
+
+const gameFinished = () =>{
+  return score === 8;
+}
+
+const showResults = () =>{
+  const modal = document.querySelector(".modal")
+  const msg = document.querySelector(".msg")
+  if (gameFinished()) {
+    msg.innerHTML = `Has achieved ${score} correct answers out of ${attempts} attempts.`
+    modal.classList.remove("off")
+    modal.classList.add("on")
+  }
+}
+
+
 export const turn = (e) => {
   const currentCard = e.currentTarget;
+  console.log(currentCard.className.includes("turned"));
 
-  !buffer.receiveElement(currentCard)? false:currentCard.classList.add("flipped");
-  
+  if (!buffer.receiveElement(currentCard) || currentCard.className.includes("turned")) {
+    return false;
+  } else {
+    currentCard.classList.add("flipped");
+  }
+
 
   if (!buffer.somePositionEmpy()) {
     if (!buffer.comparePositions()) {
-      setTimeout(() => {
-        buffer.rejected();
-        attempts++;
-        buffer.clearBuffer();
-      }, 1000);
+      solveDifference();
     }else{
-      buffer.match();
-      score++;
-      attempts++;
-      buffer.clearBuffer();
+      solveCoincidence();
+      showResults();
     }
   }
-  
-
-  showAttemps.innerHTML = `Attempts: ${attempts}`
-  showScore.innerHTML = `Score: ${score}`
 
 }
